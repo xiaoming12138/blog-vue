4b80dc93a3fa7cc1ec9bdb512025e7a159fc5243
ming.li <ming.li@roboofood.com>
2022-04-26 13:06:29 +0800

    fix: ip

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 export default {
   // 请求地址设置
   baseUrl: {
-    dev: 'http://0.0.0.0:3000/',
-    prod: 'http://0.0.0.0:3000/',
+    dev: 'http://180.76.108.65:3000/',
+    prod: 'http://180.76.108.65:3000/',
     mock: 'https://www.fastmock.site/mock/2f572a6975b9cc96b054aba2fa94c30f/api/'
   },
   // 是否开启全局mock状态
