867b98cb75c3de5c22ce7215be40a3b63422253a
ming.li <ming.li@roboofood.com>
2022-09-08 18:16:03 +0800

    fix: api

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 export default {
   // 请求地址设置
   baseUrl: {
-    dev: 'http://116.205.136.108:3000/',
-    prod: 'http://116.205.136.108:3000/',
+    dev: 'http://60.205.170.188:3000/',
+    prod: 'http://60.205.170.188:3000/',
     mock: 'https://www.fastmock.site/mock/2f572a6975b9cc96b054aba2fa94c30f/api/'
   },
   // 是否开启全局mock状态
@@ -11,7 +11,7 @@ export default {
   stroageSpace: 'community',
   // webSocket链接地址设置
   socketUrl: {
-    dev: 'ws://116.205.136.108:3001',
-    prod: 'ws://116.205.136.108:3001'
+    dev: 'ws://60.205.170.188:3001',
+    prod: 'ws://60.205.170.188:3001'
   }
 }
